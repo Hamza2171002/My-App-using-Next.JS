bf721c25bfe3f342b608cf7fa5edcf0fa82696a4
hamza217002 <muhammadhamza9191@gmail.com>
2024-10-02 12:53:32 +0500

    first commit

---
+++ my-app/src/app/about/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from '../components/navbar'
+
+const About = () => {
+    return (
+        <div>
+            <div>
+                <Navbar />
+                <h1 className='text-2xl items-center font-bold mt-20 text-center bg-green-700 text-white'>Components:</h1>
+                <p className='text-center justify-around'>In Next.js, components are reusable building blocks of your application. They allow you to break down your UI into smaller, manageable pieces. Components in Next.js are essentially JavaScript or TypeScript functions that return JSX (JavaScript XML), which is then rendered as HTML on the page.</p>
+                <h1 className='text-2xl font-bold mt-20 text-center bg-green-700 text-white'>Linking:</h1>
+                <p className=' text-center justify-around'>In Next.js, linking refers to the process of navigating between pages in a Next.js application. Next.js provides a special component called <b>Link</b> from the next/link package that allows for client-side navigation between different pages, improving performance by avoiding full page reloads.</p>
+                <h1 className='text-2xl font-bold mt-20 text-center bg-green-700 text-white'>Rounting</h1>
+                <p className=' text-center justify-around'>In Next.js, routing refers to the way the framework manages navigation between different pages in a web application. Next.js provides a file-based routing system, which means that the structure of your file system in the pages directory determines the routes of your application.</p>
+            </div>
+            <p className='text-center mt-14 bg-blue-100'>&copy;Muhammad Hamza</p>
+        </div>
+    )
+}
+
+export default About
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import Navbar from './components/navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <div className='text-center w-[100%] mt-20'>
+      <h1 className='text-2xl font-bold'>4th Week Assignment</h1>
+      <p className='text-2xl'>Given by Miss Hina Naseer Ahmad</p>
+      <h1 className='text-3xl font-bold'>Next.Js Components / Linking / Routing</h1>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
